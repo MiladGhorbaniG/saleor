1b26558947ad8735ae7d6bf2599934e4529de030
MiladGhorbaniG <47980221+MiladGhorbaniG@users.noreply.github.com>
2023-09-29 19:19:35 +0330

    update model

--- saleor/graphql/attribute/enums.py
+++ saleor/graphql/attribute/enums.py
@@ -2,20 +2,48 @@ import graphene
 
 from ...attribute import AttributeEntityType, AttributeInputType, AttributeType
 from ..core.doc_category import DOC_CATEGORY_ATTRIBUTES
-from ..core.enums import to_enum
 from ..core.utils import str_to_enum
 
-AttributeInputTypeEnum = to_enum(AttributeInputType)
-AttributeInputTypeEnum.doc_category = DOC_CATEGORY_ATTRIBUTES
+# Create a GraphQL enum for AttributeInputType
+class AttributeInputTypeEnum(graphene.Enum):
+    STRING = "STRING"
+    INTEGER = "INTEGER"
+    FLOAT = "FLOAT"
 
-AttributeTypeEnum = to_enum(AttributeType)
-AttributeTypeEnum.doc_category = DOC_CATEGORY_ATTRIBUTES
+    # Add more attribute input types as needed
 
-AttributeEntityTypeEnum = to_enum(AttributeEntityType)
-AttributeEntityTypeEnum.doc_category = DOC_CATEGORY_ATTRIBUTES
+    class Meta:
+        description = "Attribute input types."
+        doc_category = DOC_CATEGORY_ATTRIBUTES
 
-AttributeEntityTypeEnum = graphene.Enum(
-    "AttributeEntityTypeEnum",
-    [(str_to_enum(name.upper()), code) for code, name in AttributeEntityType.CHOICES],
-)
-AttributeEntityTypeEnum.doc_category = DOC_CATEGORY_ATTRIBUTES
+# Create a GraphQL enum for AttributeType
+class AttributeTypeEnum(graphene.Enum):
+    PRODUCT = "PRODUCT"
+    VARIANT = "VARIANT"
+
+    # Add more attribute types as needed
+
+    class Meta:
+        description = "Attribute types."
+        doc_category = DOC_CATEGORY_ATTRIBUTES
+
+# Create a GraphQL enum for AttributeEntityType
+class AttributeEntityTypeEnum(graphene.Enum):
+    PRODUCT = "PRODUCT"
+    VARIANT = "VARIANT"
+
+    # Add more attribute entity types as needed
+
+    class Meta:
+        description = "Attribute entity types."
+        doc_category = DOC_CATEGORY_ATTRIBUTES
+
+# Optionally, you can set descriptions and doc categories for each enum.
+AttributeInputTypeEnum._meta.description = "Attribute input types."
+AttributeTypeEnum._meta.description = "Attribute types."
+AttributeEntityTypeEnum._meta.description = "Attribute entity types."
+
+# Map Django enum choices to GraphQL enum values for AttributeEntityTypeEnum
+AttributeEntityTypeEnum._meta.enum = {
+    str_to_enum(name.upper()): code for code, name in AttributeEntityType.CHOICES
+}
