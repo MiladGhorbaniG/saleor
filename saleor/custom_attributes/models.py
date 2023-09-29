c4594da9c2239d572c3187a7ad007ec8d6e69f36
MiladGhorbaniG <47980221+MiladGhorbaniG@users.noreply.github.com>
2023-09-29 19:27:44 +0330

    update model enum

--- saleor/custom_attributes/models.py
+++ saleor/custom_attributes/models.py
@@ -4,14 +4,17 @@ from ..graphql.attribute.enums import AttributeInputTypeEnum, AttributeEntityTyp
 class CustomAttribute(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
+
+    # Convert the enum to a list of tuples for choices
     input_type = models.CharField(
         max_length=50,
-        choices=[(choice.value, choice.name) for choice in AttributeInputTypeEnum],
+        choices=AttributeInputTypeEnum.get_choices(),
     )
     entity_type = models.CharField(
         max_length=50,
-        choices=[(choice.value, choice.name) for choice in AttributeEntityTypeEnum],
+        choices=AttributeEntityTypeEnum.get_choices(),
     )
+
     unit = models.CharField(max_length=50, blank=True, null=True)
     choices = models.JSONField(default=list)  # Store choices as JSON data
 
