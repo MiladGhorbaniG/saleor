6d110678ed3340f64516553ae68815af90190b2d
MiladGhorbaniG <47980221+MiladGhorbaniG@users.noreply.github.com>
2023-09-29 19:28:46 +0330

    update model enum

--- saleor/custom_attributes/models.py
+++ saleor/custom_attributes/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from ..graphql.attribute.enums import AttributeInputTypeEnum, AttributeEntityTypeEnum
 
+# Function to convert GraphQL enum to Django model choices
+def get_input_type_choices():
+    return [(choice.name, choice.name) for choice in AttributeInputTypeEnum]
+
+def get_entity_type_choices():
+    return [(choice.name, choice.name) for choice in AttributeEntityTypeEnum]
+
 class CustomAttribute(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
-    # Convert the enum to a list of tuples for choices
+    # Use the functions to get choices
     input_type = models.CharField(
         max_length=50,
-        choices=AttributeInputTypeEnum.get_choices(),
+        choices=get_input_type_choices(),
     )
     entity_type = models.CharField(
         max_length=50,
-        choices=AttributeEntityTypeEnum.get_choices(),
+        choices=get_entity_type_choices(),
     )
 
     unit = models.CharField(max_length=50, blank=True, null=True)
