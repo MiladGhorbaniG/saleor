63489a9cde156303897e4675ad9183eafae05408
MiladGhorbaniG <47980221+MiladGhorbaniG@users.noreply.github.com>
2023-09-29 19:18:00 +0330

    update model

--- saleor/custom_attributes/models.py
+++ saleor/custom_attributes/models.py
@@ -6,11 +6,11 @@ class CustomAttribute(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     input_type = models.CharField(
         max_length=50,
-        choices=AttributeInputTypeEnum.CHOICES,
+        choices=[(choice.value, choice.name) for choice in AttributeInputTypeEnum],
     )
     entity_type = models.CharField(
         max_length=50,
-        choices=AttributeEntityTypeEnum.CHOICES,
+        choices=[(choice.value, choice.name) for choice in AttributeEntityTypeEnum],
     )
     unit = models.CharField(max_length=50, blank=True, null=True)
     choices = models.JSONField(default=list)  # Store choices as JSON data
